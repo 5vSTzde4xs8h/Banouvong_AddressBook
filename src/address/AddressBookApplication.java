30f6a48e2a41b6a26cce0ea67947efb668462734
5vSTzde4xs8h <124944815+5vSTzde4xs8h@users.noreply.github.com>
2024-02-13 15:27:45 -0800

    Reformat code

--- src/address/AddressBookApplication.java
+++ src/address/AddressBookApplication.java
@@ -5,40 +5,47 @@ import address.data.AddressEntry;
 
 /**
  * Address book driver
+ *
  * @author Poleon Banouvong
  * @since 2024-01-25
  * @version 1.0
  */
 public class AddressBookApplication {
-    /**
-     * Creates two address book entries and places them into
-     * an address book
-     * @param ab The address book the entries will be placed in
-     */
-    public static void initAddressBookExercise(AddressBook ab) {
-        AddressEntry ae1 = new AddressEntry(
-                "John", "Doe",
-                "123 Main Street", "Main Town", 12345,
-                "123-456-789",
-                "john_doe@example.com"
-        );
+  /**
+   * Creates two address book entries and places them into an address book
+   *
+   * @param ab The address book the entries will be placed in
+   */
+  public static void initAddressBookExercise(AddressBook ab) {
+    AddressEntry ae1 =
+        new AddressEntry(
+            "John",
+            "Doe",
+            "123 Main Street",
+            "Main Town",
+            12345,
+            "123-456-789",
+            "john_doe@example.com");
 
-        AddressEntry ae2 = new AddressEntry(
-                "Jane", "Doe",
-                "456 Main Street", "Main Town", 12345,
-                "987-654-321",
-                "jane_doe@example.com"
-        );
+    AddressEntry ae2 =
+        new AddressEntry(
+            "Jane",
+            "Doe",
+            "456 Main Street",
+            "Main Town",
+            12345,
+            "987-654-321",
+            "jane_doe@example.com");
 
-        // add entries
-        ab.add(ae1);
-        ab.add(ae2);
+    // add entries
+    ab.add(ae1);
+    ab.add(ae2);
 
-        ab.list();  // print out the address book
-    }
-    
-    public static void main(String[] args) {
-        AddressBook addressBook = new AddressBook();
-        initAddressBookExercise(addressBook);
-    }
+    ab.list(); // print out the address book
+  }
+
+  public static void main(String[] args) {
+    AddressBook addressBook = new AddressBook();
+    initAddressBookExercise(addressBook);
+  }
 }
