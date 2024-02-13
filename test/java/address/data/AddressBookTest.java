b95f7b7b9386cac440162efd28773201153ad61e
5vSTzde4xs8h <124944815+5vSTzde4xs8h@users.noreply.github.com>
2024-02-13 14:44:16 -0800

    Add JUnit testing

---
+++ test/java/address/data/AddressBookTest.java
@@ -0,0 +1,15 @@
+package address.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookTest {
+  @Test
+  void list() {
+  }
+
+  @Test
+  void add() {
+  }
+}
