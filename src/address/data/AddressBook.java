2b4c3ea3661a6121a449b4bc3c1a9fd831585332
5vSTzde4xs8h <124944815+5vSTzde4xs8h@users.noreply.github.com>
2024-02-16 19:45:35 -0800

    Update AddressBook implementation and tests

--- src/address/data/AddressBook.java
+++ src/address/data/AddressBook.java
@@ -1,38 +1,76 @@
 package address.data;
 
-import java.util.LinkedHashSet;
+import java.util.ArrayList;
+import java.util.TreeSet;
 
 /**
  * Holds a list of address entries
  *
  * @author Poleon Banouvong
  * @since 2024-01-25
- * @version 1.0
  */
 public class AddressBook {
-  private final LinkedHashSet<AddressEntry> addressEntryList;
+  private final TreeSet<AddressEntry> addressEntryList;
 
   /** Creates a new address book */
   public AddressBook() {
-    addressEntryList = new LinkedHashSet<AddressEntry>();
-  }
-
-  /** Prints out all address book entries */
-  public void list() {
-    int i = 1;
-
-    for (AddressEntry addressEntry : addressEntryList) {
-      System.out.println(i + ": " + addressEntry.toString() + '\n');
-      i++;
-    }
+    addressEntryList = new TreeSet<AddressEntry>();
   }
 
   /**
    * Adds an address entry to the address book
    *
-   * @param addressEntry The address entry
+   * @param addressEntry The address entry to add
+   * @return {@code true} if the address entry was not already in the address book
    */
-  public void add(AddressEntry addressEntry) {
-    addressEntryList.add(addressEntry);
+  public boolean add(AddressEntry addressEntry) {
+    return addressEntryList.add(addressEntry);
+  }
+
+  /**
+   * Removes an address entry from the address book
+   *
+   * @param addressEntry The address entry to remove
+   * @return {@code true} if the address entry was in the address book
+   */
+  public boolean remove(AddressEntry addressEntry) {
+    return addressEntryList.remove(addressEntry);
+  }
+
+  /** Prints out all address book entries */
+  public String getListing() {
+    int addressNum = 1;
+    String listing = "";
+
+    for (AddressEntry addressEntry : addressEntryList) {
+      listing += (addressNum + ": " + addressEntry.toString() + '\n');
+      addressNum++;
+    }
+
+    return listing;
+  }
+
+  /**
+   * TODO: Implement addEntriesFromFile
+   * Adds and returns a list of address entries read from a file
+   *
+   * @param fileName The file to read address entries from
+   * @return An {@link ArrayList} containing the added entries
+   */
+  public ArrayList<AddressEntry> addEntriesFromFile(String fileName) {
+    ArrayList<AddressEntry> addressEntries = new ArrayList<AddressEntry>();
+    return addressEntries;
+  }
+
+  /**
+   * TODO: Implement findEntriesWithLastNameStartingWith
+   * Returns a list of address entries whose last name starts with a prefix
+   *
+   * @param prefix The prefix to match last names against
+   * @return An {@link ArrayList} containing the matching entries
+   */
+  public ArrayList<AddressEntry> findEntriesWithLastNameStartingWith(String prefix) {
+    ArrayList<AddressEntry> addressEntries = new ArrayList<AddressEntry>();
+    return addressEntries;
   }
 }
--- test/java/address/data/AddressBookTest.java
+++ test/java/address/data/AddressBookTest.java
@@ -1,5 +1,6 @@
 package address.data;
 
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,13 +12,186 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 2024-02-13
  */
 class AddressBookTest {
+  /** Address entry for John Doe */
+  AddressEntry johnDoe =
+      new AddressEntry(
+          "John",
+          "Doe",
+          "1234 Main Street",
+          "Maintown",
+          12345,
+          "1234567890",
+          "johndoe@example.com");
+
+  /** Address entry for Jane Doe */
+  AddressEntry janeDoe =
+      new AddressEntry(
+          "Jane",
+          "Doe",
+          "5678 Main Street",
+          "Maintown",
+          12345,
+          "0987654321",
+          "janedoe@example.com");
+
+  /** Address entry for John Smith */
+  AddressEntry johnSmith =
+      new AddressEntry(
+          "John",
+          "Smith",
+          "9012 Main Street",
+          "Maintown",
+          12345,
+          "2468013579",
+          "johnsmith@example.com");
+
+  /** Address entry for a second but distinct John Doe */
+  AddressEntry johnDoe2 =
+      new AddressEntry(
+          "John",
+          "Doe",
+          "3456 Main Street",
+          "Maintown",
+          12345,
+          "1357924680",
+          "johndoe2@example.com");
+
+  /** Tests that an {@link AddressEntry} can be added to the address book */
   @Test
-  void list() {
+  public void testAdd() {
+    AddressBook addressBook = new AddressBook();
+    assertTrue(addressBook.add(johnDoe));
+  }
+
+  /** Tests that adding the same {@link AddressEntry} twice doesn't work */
+  @Test
+  public void testAddDuplicate() {
+    AddressBook addressBook = new AddressBook();
+    addressBook.add(johnDoe);
+
+    assertFalse(addressBook.add(johnDoe));
+  }
+
+  /** Tests that adding two {@link AddressEntry}s with the same first name works */
+  @Test
+  public void testAddDuplicateFirstName() {
+    AddressBook addressBook = new AddressBook();
+    addressBook.add(johnDoe);
+
+    assertTrue(addressBook.add(johnSmith));
+  }
+
+  /** Tests that adding two {@link AddressEntry}s with the same last name works */
+  @Test
+  public void testAddDuplicateLastName() {
+    AddressBook addressBook = new AddressBook();
+    addressBook.add(johnDoe);
+
+    assertTrue(addressBook.add(janeDoe));
+  }
+
+  /**
+   * Tests that adding two different {@link AddressEntry}s with the same first and last name doesn't
+   * work
+   */
+  @Test
+  public void testAddDuplicateNames() {
+    AddressBook addressBook = new AddressBook();
+    addressBook.add(johnDoe);
+
+    assertFalse(addressBook.add(johnDoe2));
+  }
+
+  /** Tests that address entries can be read from a file */
+  @Test
+  public void testReadFile() {
     fail("Not implemented");
   }
 
+  /**
+   * Tests that reading entries from a malformed file will add as many entries as possible up to the
+   * first malformed entry
+   */
   @Test
-  void add() {
+  public void testReadMalformedFile() {
     fail("Not implemented");
+  }
+
+  /** Tests that reading from an empty file adds no entries */
+  @Test
+  public void testReadEmptyFile() {
+    fail("Not implemented");
+  }
+
+  /** Tests that an {@link AddressEntry} can be removed from an address book */
+  @Test
+  public void testRemove() {
+    AddressBook addressBook = new AddressBook();
+    addressBook.add(johnDoe);
+
+    assertTrue(addressBook.remove(johnDoe));
+  }
+
+  /** Tests that removing an {@link AddressEntry} twice from the same address book doesn't work */
+  @Test
+  public void testRemoveDuplicate() {
+    AddressBook addressBook = new AddressBook();
+    addressBook.add(johnDoe);
+    addressBook.remove(johnDoe);
+
+    assertFalse(addressBook.remove(johnDoe));
+  }
+
+  /** Tests that finding {@link AddressEntry}s by last name works */
+  @Test
+  public void testFind() {
+    AddressBook addressBook = new AddressBook();
+    addressBook.add(johnDoe);
+    addressBook.add(janeDoe);
+
+    ArrayList<AddressEntry> foundEntriesFullLastName =
+        addressBook.findEntriesWithLastNameStartingWith("Doe");
+    ArrayList<AddressEntry> foundEntriesPartialLastName =
+        addressBook.findEntriesWithLastNameStartingWith("D");
+
+    assertEquals(2, foundEntriesFullLastName.size());
+    assertEquals(2, foundEntriesPartialLastName.size());
+  }
+
+  /** Tests that finding {@link AddressEntry}s is case-insensitive */
+  @Test
+  public void testFindCaseInsensitive() {
+    AddressBook addressBook = new AddressBook();
+    addressBook.add(johnDoe);
+    addressBook.add(janeDoe);
+
+    ArrayList<AddressEntry> foundEntries = addressBook.findEntriesWithLastNameStartingWith("DOE");
+    ArrayList<AddressEntry> foundEntries2 = addressBook.findEntriesWithLastNameStartingWith("DoE");
+    ArrayList<AddressEntry> foundEntries3 = addressBook.findEntriesWithLastNameStartingWith("dOe");
+    ArrayList<AddressEntry> foundEntries4 = addressBook.findEntriesWithLastNameStartingWith("dOE");
+
+    assertEquals(2, foundEntries.size());
+    assertEquals(2, foundEntries2.size());
+    assertEquals(2, foundEntries3.size());
+    assertEquals(2, foundEntries4.size());
+  }
+
+  /** Tests that address listing works */
+  @Test
+  public void testListing() {
+    AddressBook addressBook = new AddressBook();
+    addressBook.add(johnDoe);
+    addressBook.add(janeDoe);
+
+    String addressListing = addressBook.getListing();
+    assertTrue(addressListing.contains(johnDoe.toString()));
+    assertTrue(addressListing.contains(janeDoe.toString()));
+  }
+
+  /** Tests that address listing an empty address book returns an empty string */
+  @Test
+  public void testListingEmpty() {
+    AddressBook addressBook = new AddressBook();
+    assertEquals("", addressBook.getListing());
   }
 }
